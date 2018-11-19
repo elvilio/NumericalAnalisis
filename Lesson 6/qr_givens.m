fb0ae5ee57fedea23d38b6ee0d412690ea497bab
elvilio <elvilio98@gmail.com>
2018-11-19 15:30:33 +0100

    correction

--- Lesson 6/qr_givens.m
+++ Lesson 6/qr_givens.m
@@ -3,17 +3,10 @@ function [Q,R] = qr_givens (A)
   Q = eye(n);
   for i=1:n-1
     G = eye(n);
-    G(n-i:n,n-i:n) = my_givens_vec(A(n-i:n,i))
-    G*A
-    A=G'*A;
+    G(i:n,i:n) = my_givens_vec(A(i:n,i));
+    A=G*A;
     Q=Q*G;
-    
-    %for j=n:-1:i+1
-    %  G = eye(n);
-    %  G([j,i],[j,i]) = mygivens([A(j,i),A(j,i)]');
-    %  A=G'*A;
-    %  Q=Q*G;
-    %endfor
   endfor
+  %R=triu(A);
   R=A;
 endfunction
